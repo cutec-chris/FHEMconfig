815a11155a3e0e632557d2853f2c018701437dce
Christian U <info@cu-tec.de>
2015-03-12 23:14:17 +0100

    more Icons for devices

--- source/uicons.pas
+++ source/uicons.pas
@@ -13,12 +13,20 @@ const
                +'jeelink:8'+#10
                +'apptime:9'+#10
                +'at:10'+#10
+               +'notify:32'+#10
                +'autocreate:11'+#10
                +'backup:12'+#10
                +'configdb:13'+#10
                +'web:14'+#10
+
                +'twilight:15'+#10
                +'weather:15'+#10
+               +'netatmo:15'+#10
+               +'weco:15'+#10
+               +'all4000t:15'+#10
+
+               +'rain:35'+#10
+
                +'presence:16'+#10
                +'doif:17'+#10
                +'filelog:18'+#10
@@ -33,13 +41,28 @@ const
                +'gpio4:26'+#10
 
                +'lacrosse:27'+#10
-               +'KM200:27'+#10
+               +'km200:27'+#10
                +'cul_hm:27'+#10
 
                +'wol:28'+#10
+               +'speedtest:28'+#10
+
                +'harmony:29'+#10
+               +'remotecontrol:29'+#10
+
                +'holiday':30'+#10
                +'mailcheck:31'+#10
+
+               +'pilight:33'+#10
+               +'all3076:33'+#10
+
+               +'withings:36'+#10
+
+               +'xxlg7000:37'+#10
+
+               +'all4027:38'+#10
+
+               +'alarm:39'+#10
                ;
 
 implementation
